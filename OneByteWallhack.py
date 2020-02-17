045545ed018b86e4f5757b19832b874aa5708c1a
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-17 20:51:40 +0100

    Add code

---
+++ OneByteWallhack.py
@@ -0,0 +1,13 @@
+import pymem
+import re
+
+pm = pymem.Pymem('csgo.exe')
+client = pymem.process.module_from_name(pm.process_handle,
+                                        'client_panorama.dll')
+
+clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
+                                         clientModule).start() + 2
+
+pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
+pm.close_process()
