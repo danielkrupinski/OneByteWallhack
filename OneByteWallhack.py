5b4fd715dc9df5aee20cd02bbdec079369354a36
davidgfb <davidgracia99@gmail.com>
2020-10-24 10:24:19 +0200

    Update OneByteWallhack.py

--- OneByteWallhack.py
+++ OneByteWallhack.py
@@ -1,12 +1,12 @@
-import pymem
-import re
+from pymem import Pymem,process
+from re import search
 
-pm = pymem.Pymem('csgo.exe')
-client = pymem.process.module_from_name(pm.process_handle,
+pm = Pymem('csgo.exe')
+client = process.module_from_name(pm.process_handle,
                                         'client.dll')
 
 clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
+address = client.lpBaseOfDll + search(rb'\x83\xF8.\x8B\x45\x08\x0F',
                                          clientModule).start() + 2
 
 pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
