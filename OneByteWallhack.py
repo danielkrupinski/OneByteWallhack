c4a54a1665e8551eb5c161eacdd695dcc396856c
davidgfb <davidgracia99@gmail.com>
2020-11-28 23:58:29 +0100

    add exception management to avoid errors when process is not launched

--- OneByteWallhack.py
+++ OneByteWallhack.py
@@ -1,13 +1,19 @@
-import pymem
-import re
+from pymem import Pymem,process
+from re import search
 
-pm = pymem.Pymem('csgo.exe')
-client = pymem.process.module_from_name(pm.process_handle,
-                                        'client.dll')
+try:
+    processName='csgo.exe'
+    pm = Pymem(processName)
+    client = process.module_from_name(pm.process_handle,'client.dll')
 
-clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
-                                         clientModule).start() + 2
+    clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+    address = client.lpBaseOfDll + search(rb'\x83\xF8.\x8B\x45\x08\x0F',
+                                          clientModule).start() + 2
 
-pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
-pm.close_process()
+    pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
+    pm.close_process()
+
+    print("hack completed")
+    
+except:
+    print("error: couldn't find process",processName)
