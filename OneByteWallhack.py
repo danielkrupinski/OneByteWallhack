de77e8db6c5d455e2eaa3882f5e2898f91f1156f
Daniel Krupiński <danielkrupinski@outlook.com>
2022-10-22 19:22:09 +0200

    Update memory pattern for the latest game update

--- OneByteWallhack.py
+++ OneByteWallhack.py
@@ -6,8 +6,8 @@ client = pymem.process.module_from_name(pm.process_handle,
                                         'client.dll')
 
 clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
-                                         clientModule).start() + 2
+address = client.lpBaseOfDll + re.search(rb'\x33\xC0\x83\xFA.\xB9\x20',
+                                         clientModule).start() + 4
 
 pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
 pm.close_process()
